f8fa51d7f13f8a64fcdc8aebe2d1d1723d064e93
Henrique Martins <hmartins224@gmail.com>
2020-06-14 15:07:37 -0300

    Changing button names

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,16 +28,16 @@
             System.Windows.Forms.Label nm_loginLabel;
             System.Windows.Forms.Label passwordLabel;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
-            this.btn7 = new System.Windows.Forms.Button();
-            this.btn8 = new System.Windows.Forms.Button();
-            this.btn9 = new System.Windows.Forms.Button();
-            this.btn10 = new System.Windows.Forms.Button();
-            this.btn11 = new System.Windows.Forms.Button();
-            this.btn12 = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnPrintOut = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.database1DataSet1 = new WindowsFormsApp1.Database1DataSet();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.tb_userBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -103,105 +103,105 @@
             passwordLabel.TabIndex = 18;
             passwordLabel.Text = "Senha";
             // 
-            // btn3
+            // btnPrevious
             // 
-            this.btn3.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn3.Location = new System.Drawing.Point(224, 283);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(75, 23);
-            this.btn3.TabIndex = 0;
-            this.btn3.Text = "Anterior";
-            this.btn3.UseVisualStyleBackColor = true;
+            this.btnPrevious.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnPrevious.Location = new System.Drawing.Point(224, 283);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "Anterior";
+            this.btnPrevious.UseVisualStyleBackColor = true;
             // 
-            // btn4
+            // btnNext
             // 
-            this.btn4.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn4.Location = new System.Drawing.Point(389, 283);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(75, 23);
-            this.btn4.TabIndex = 0;
-            this.btn4.Text = "Próximo";
-            this.btn4.UseVisualStyleBackColor = true;
+            this.btnNext.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnNext.Location = new System.Drawing.Point(389, 283);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Próximo";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
-            // btn5
+            // btnNew
             // 
-            this.btn5.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn5.Location = new System.Drawing.Point(470, 283);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(75, 23);
-            this.btn5.TabIndex = 0;
-            this.btn5.Text = "Novo";
-            this.btn5.UseVisualStyleBackColor = true;
+            this.btnNew.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnNew.Location = new System.Drawing.Point(470, 283);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "Novo";
+            this.btnNew.UseVisualStyleBackColor = true;
             // 
-            // btn6
+            // btnChange
             // 
-            this.btn6.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn6.Location = new System.Drawing.Point(305, 283);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(75, 23);
-            this.btn6.TabIndex = 0;
-            this.btn6.Text = "Alterar";
-            this.btn6.UseVisualStyleBackColor = true;
+            this.btnChange.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnChange.Location = new System.Drawing.Point(305, 283);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "Alterar";
+            this.btnChange.UseVisualStyleBackColor = true;
             // 
-            // btn7
+            // btnSave
             // 
-            this.btn7.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn7.Location = new System.Drawing.Point(327, 213);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(75, 23);
-            this.btn7.TabIndex = 0;
-            this.btn7.Text = "Salvar";
-            this.btn7.UseVisualStyleBackColor = true;
+            this.btnSave.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnSave.Location = new System.Drawing.Point(327, 213);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
-            // btn8
+            // btnCancel
             // 
-            this.btn8.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn8.Location = new System.Drawing.Point(143, 283);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(75, 23);
-            this.btn8.TabIndex = 0;
-            this.btn8.Text = "Cancelar";
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnCancel.Location = new System.Drawing.Point(143, 283);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // btn9
+            // btnSearch
             // 
-            this.btn9.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn9.Location = new System.Drawing.Point(519, 12);
-            this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(75, 23);
-            this.btn9.TabIndex = 0;
-            this.btn9.Text = "Pesquisar";
-            this.btn9.UseVisualStyleBackColor = true;
+            this.btnSearch.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnSearch.Location = new System.Drawing.Point(519, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Pesquisar";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // btn10
+            // btnPrintOut
             // 
-            this.btn10.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn10.Location = new System.Drawing.Point(12, 12);
-            this.btn10.Name = "btn10";
-            this.btn10.Size = new System.Drawing.Size(75, 23);
-            this.btn10.TabIndex = 0;
-            this.btn10.Text = "Imprimir";
-            this.btn10.UseVisualStyleBackColor = true;
+            this.btnPrintOut.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnPrintOut.Location = new System.Drawing.Point(12, 12);
+            this.btnPrintOut.Name = "btnPrintOut";
+            this.btnPrintOut.Size = new System.Drawing.Size(75, 23);
+            this.btnPrintOut.TabIndex = 0;
+            this.btnPrintOut.Text = "Imprimir";
+            this.btnPrintOut.UseVisualStyleBackColor = true;
             // 
-            // btn11
+            // btnExit
             // 
-            this.btn11.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn11.Location = new System.Drawing.Point(62, 283);
-            this.btn11.Name = "btn11";
-            this.btn11.Size = new System.Drawing.Size(75, 23);
-            this.btn11.TabIndex = 0;
-            this.btn11.Text = "Sair";
-            this.btn11.UseVisualStyleBackColor = true;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnExit.Location = new System.Drawing.Point(62, 283);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Sair";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
-            // btn12
+            // btnDelete
             // 
-            this.btn12.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.btn12.Location = new System.Drawing.Point(246, 213);
-            this.btn12.Name = "btn12";
-            this.btn12.Size = new System.Drawing.Size(75, 23);
-            this.btn12.TabIndex = 0;
-            this.btn12.Text = "Excluir";
-            this.btn12.UseVisualStyleBackColor = true;
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnDelete.Location = new System.Drawing.Point(246, 213);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // database1DataSet1
             // 
@@ -278,16 +278,16 @@
             this.Controls.Add(this.nm_loginTextBox);
             this.Controls.Add(passwordLabel);
             this.Controls.Add(this.passwordTextBox);
-            this.Controls.Add(this.btn4);
-            this.Controls.Add(this.btn12);
-            this.Controls.Add(this.btn11);
-            this.Controls.Add(this.btn10);
-            this.Controls.Add(this.btn9);
-            this.Controls.Add(this.btn8);
-            this.Controls.Add(this.btn7);
-            this.Controls.Add(this.btn6);
-            this.Controls.Add(this.btn5);
-            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrintOut);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnPrevious);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -308,16 +308,16 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button btn3;
-        private System.Windows.Forms.Button btn4;
-        private System.Windows.Forms.Button btn5;
-        private System.Windows.Forms.Button btn6;
-        private System.Windows.Forms.Button btn7;
-        private System.Windows.Forms.Button btn8;
-        private System.Windows.Forms.Button btn9;
-        private System.Windows.Forms.Button btn10;
-        private System.Windows.Forms.Button btn11;
-        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnPrintOut;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDelete;
         private Database1DataSet database1DataSet1;
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.BindingSource tb_userBindingSource;
